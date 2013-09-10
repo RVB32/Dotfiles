3dede1feee614d4defef195a85a96c1db2da88c5
Richard M. Van Buren <vanburen.rich@gmail.com>
2013-09-10 10:53:15 -0400

    backups Dotfiles before overwriting them

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 echo "\033[0;34mSetting up basic Dot Files\033[0m"
 
+echo "\033[0;34mLooking for existing DotFiles...\033[0m"
+if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
+then
+  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.BackUpzshrc\033[0m";
+  mv ~/.zshrc ~/.BackUpzshrc;
+elif [ -f ~/.vimrc ] || [ -h ~/.vimrc ]
+then
+  echo "\033[0;33mFound ~/.vimrc.\033[0m \033[0;32mBacking up to ~/.BackUpvimrc\033[0m";
+  mv ~/.zshrc ~/.BackUpvimrc;
+elif [ -f ~/.gitconfig ] || [ -h ~/.gitconfig ]
+then
+  echo "\033[0;33mFound ~/.gitconfig.\033[0m \033[0;32mBacking up to ~/.BackUpgitconfig\033[0m";
+  mv ~/.zshrc ~/.BackUpgitconfig;
+elif [ -f ~/.aliases ] || [ -h ~/.aliases ]
+then
+  echo "\033[0;33mFound ~/.aliases.\033[0m \033[0;32mBacking up to ~/.BackUpaliases\033[0m";
+  mv ~/.zshrc ~/.BackUpaliases;
+
+echo "\033[0;34mDone Copying DotFiles\033[0m"
+
 echo "\033[0;34mcopying zshrc file to home directory\033[0m"
 cp ~/Dotfiles/.zshrc ~/.zshrc
 
